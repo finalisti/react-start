6ea668770d3e208dd723dfcad42627957d13009f
finalist <lauri.laaksonen11@gmail.com>
2026-01-25 22:16:40 +0200

    fixed main and app files

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
-import './App.css';
-const App = () => {
-  return (
-    <>
-      <h1>My App</h1>
-    </>
-  );
-};
-export default App;
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
